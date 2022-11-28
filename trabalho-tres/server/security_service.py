26f756a50b2a46fe3fb8e6d8a2b05e9b02ed2c4a
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2022-11-27 23:05:52 -0300

    Added message signature

--- trabalho-tres/server/security_service.py
+++ trabalho-tres/server/security_service.py
@@ -1,10 +1,16 @@
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pss
+
+
 class SecurityService():
     def __init__(self) -> None:
-        self.privateKey = "TODO"
-        self.publicKey = "TODO"
+        self.keyPair = RSA.generate(1024)
 
     def get_public_key(self):
-        return self.publicKey
+        return self.keyPair.publickey().export_key()
 
-    def create_signature(self, client_name):
-        return "SIGNATURE"
+    def create_signature(self, client_name: str):
+        message = client_name.encode("utf-8")
+        hash = SHA256.new(message)
+        return pss.new(self.keyPair).sign(hash)
