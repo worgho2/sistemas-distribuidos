e3ac14db8b2beee594122c6ced68264194b4e1d0
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2022-11-28 12:29:35 -0300

    Added invite answer and appointment reminder edit

--- trabalho-tres/client/components/pages/appointments/table/row/index.tsx
+++ trabalho-tres/client/components/pages/appointments/table/row/index.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import { IconButton, Stack, TableCell, TableRow, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    FormControl,
+    IconButton,
+    InputLabel,
+    MenuItem,
+    Select,
+    Stack,
+    TableCell,
+    TableRow,
+    Typography,
+} from '@mui/material';
 import { useCalendarContext } from '../../../../../hooks/calendar-provider';
-import { Appointment } from '../../../../../models/appointment';
+import { Appointment, Reminder } from '../../../../../models/appointment';
 import { DeleteForever, Edit } from '@mui/icons-material';
+import { Box } from '@mui/system';
 
 interface AppointmentsPageTableRowProps {
     appointment: Appointment;
@@ -11,6 +27,8 @@ interface AppointmentsPageTableRowProps {
 const AppointmentsPageTableRow: React.FC<AppointmentsPageTableRowProps> = (props) => {
     const { appointment } = props;
     const { cancelAppointment, updateAppointmentReminder } = useCalendarContext();
+    const [editOpen, setEditOpen] = useState<boolean>(false);
+    const [reminder, setReminder] = useState<Reminder>(Reminder.ON_TIME);
 
     const attendees = appointment.attendees.join(', ');
     const date = new Date(appointment.date);
@@ -29,7 +47,11 @@ const AppointmentsPageTableRow: React.FC<AppointmentsPageTableRowProps> = (props
 
                 <TableCell>
                     <Stack direction="row">
-                        <IconButton onClick={() => {}}>
+                        <IconButton
+                            onClick={() => {
+                                setEditOpen(true);
+                            }}
+                        >
                             <Edit color="info" />
                         </IconButton>
 
@@ -39,6 +61,56 @@ const AppointmentsPageTableRow: React.FC<AppointmentsPageTableRowProps> = (props
                     </Stack>
                 </TableCell>
             </TableRow>
+            <Dialog
+                open={editOpen}
+                onClose={() => {
+                    setEditOpen(false);
+                }}
+            >
+                <DialogTitle>Update {appointment.name} appointment reminder</DialogTitle>
+                <DialogContent>
+                    <Box sx={{ paddingTop: 2 }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Reminder</InputLabel>
+                            <Select
+                                value={reminder}
+                                variant="outlined"
+                                size="medium"
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                label="Reminder"
+                                onChange={(event) => {
+                                    setReminder(event.target.value as Reminder);
+                                }}
+                            >
+                                <MenuItem value={Reminder.DISABLED}>Disabled</MenuItem>
+                                <MenuItem value={Reminder.TEN_MINUTES_BEFORE}>Ten minutes before</MenuItem>
+                                <MenuItem value={Reminder.FIVE_MINUTES_BEFORE}>Five minutes before</MenuItem>
+                                <MenuItem value={Reminder.ON_TIME}>On time</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        color="inherit"
+                        onClick={() => {
+                            setEditOpen(false);
+                        }}
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        color="success"
+                        onClick={async () => {
+                            await updateAppointmentReminder(appointment.name, reminder);
+                            setEditOpen(false);
+                        }}
+                    >
+                        Update
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </React.Fragment>
     );
 };
--- trabalho-tres/client/components/pages/invites/table/row/index.tsx
+++ trabalho-tres/client/components/pages/invites/table/row/index.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
-import { IconButton, TableCell, TableRow, Typography } from '@mui/material';
-import { AppointmentInvite, AppointmentReminder } from '../../../../../models/appointment';
+import React, { useState } from 'react';
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    FormControl,
+    IconButton,
+    InputLabel,
+    MenuItem,
+    Select,
+    TableCell,
+    TableRow,
+    Typography,
+} from '@mui/material';
+import { AppointmentInvite, AppointmentReminder, Reminder } from '../../../../../models/appointment';
 import { useCalendarContext } from '../../../../../hooks/calendar-provider';
 import { Reply } from '@mui/icons-material';
 
@@ -13,6 +28,10 @@ const InvitesPageTableRow: React.FC<InvitesPageTableRowProps> = (props) => {
     const { answerAppointmentInvite } = useCalendarContext();
     const date = new Date(invite.date);
 
+    const [replyOpen, setReplyOpen] = useState<boolean>(false);
+    const [accept, setAccept] = useState<boolean>(true);
+    const [reminder, setReminder] = useState<Reminder>(Reminder.ON_TIME);
+
     return (
         <React.Fragment>
             <TableRow>
@@ -23,11 +42,90 @@ const InvitesPageTableRow: React.FC<InvitesPageTableRowProps> = (props) => {
                 </TableCell>
 
                 <TableCell>
-                    <IconButton onClick={() => {}}>
+                    <IconButton
+                        onClick={() => {
+                            setReplyOpen(true);
+                        }}
+                    >
                         <Reply color="info" />
                     </IconButton>
                 </TableCell>
             </TableRow>
+            <Dialog
+                open={replyOpen}
+                onClose={() => {
+                    setReplyOpen(false);
+                }}
+            >
+                <DialogTitle>Reply {invite.name} appointment invite</DialogTitle>
+                <DialogContent>
+                    <Box sx={{ paddingTop: 2 }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Accept</InputLabel>
+                            <Select
+                                value={accept}
+                                variant="outlined"
+                                size="medium"
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                label="Reminder"
+                                onChange={(event) => {
+                                    setAccept(event.target.value === 'true');
+                                }}
+                            >
+                                <MenuItem value={'true'}>Yes</MenuItem>
+                                <MenuItem value={'false'}>No</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+
+                    <Box sx={{ paddingTop: 2 }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Reminder</InputLabel>
+                            <Select
+                                value={reminder}
+                                variant="outlined"
+                                size="medium"
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                label="Reminder"
+                                onChange={(event) => {
+                                    setReminder(event.target.value as Reminder);
+                                }}
+                            >
+                                <MenuItem value={Reminder.DISABLED}>Disabled</MenuItem>
+                                <MenuItem value={Reminder.TEN_MINUTES_BEFORE}>Ten minutes before</MenuItem>
+                                <MenuItem value={Reminder.FIVE_MINUTES_BEFORE}>Five minutes before</MenuItem>
+                                <MenuItem value={Reminder.ON_TIME}>On time</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        color="inherit"
+                        onClick={() => {
+                            setReplyOpen(false);
+                        }}
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        color="success"
+                        onClick={async () => {
+                            if (accept === false) {
+                                await answerAppointmentInvite(invite.name, accept, Reminder.DISABLED);
+                            } else {
+                                await answerAppointmentInvite(invite.name, accept, reminder);
+                            }
+
+                            setReplyOpen(false);
+                        }}
+                    >
+                        Done
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </React.Fragment>
     );
 };
