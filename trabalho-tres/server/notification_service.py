a6c2cde5cae4cd3c7c59dd510e25449e527ab2aa
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2022-11-27 22:45:58 -0300

    Added task task scheduling

--- trabalho-tres/server/notification_service.py
+++ trabalho-tres/server/notification_service.py
@@ -1,14 +1,30 @@
 from flask_sse import sse
+from datetime import datetime, timedelta
+from threading import Timer
 from appointment import Appointment
 
 
 class NotificationService():
-    def schedule_appointment_reminder(self, client_name, appointment: Appointment, reminder: str):
-        if reminder is "DISABLED":
-            return
+    def __init__(self) -> None:
+        self.timer_tasks: dict[str, Timer] = {}
 
-        # TODO: Schedule
+    def __get_timer_task_id(self, client_name: str, appointment_name: str):
+        return f"{appointment_name}|{client_name}"
 
+    def __get_timer_task_interval(self, date: datetime, reminder: str):
+        minutes_to_subtract = 0
+
+        if reminder is "FIVE_MINUTES_BEFORE":
+            minutes_to_subtract = 5
+        elif reminder is "TEN_MINUTES_BEFORE":
+            minutes_to_subtract = 10
+
+        date_with_offset = date - timedelta(minutes=minutes_to_subtract)
+        now = datetime.now()
+
+        return (date_with_offset - now).total_seconds()
+
+    def __send_appointment_reminder(self, client_name: str, appointment: Appointment, reminder: str):
         sse.publish({
             "name": appointment.name,
             "date": appointment.date,
@@ -16,13 +32,32 @@ class NotificationService():
             "reminder": reminder
         }, type=f"reminders-{client_name}")
 
+    def schedule_appointment_reminder(self, client_name: str, appointment: Appointment, reminder: str):
+        if reminder is "DISABLED":
+            return
+
+        timer_id = self.__get_timer_task_id(client_name, appointment.name)
+        timer_interval = self.__get_timer_task_interval(
+            appointment.date, reminder)
+        timer = Timer(timer_interval, self.__send_appointment_reminder,
+                      [client_name, appointment, reminder])
+        self.timer_tasks.update({timer_id: timer})
+        self.timer_tasks.get(timer_id).start()
+
     def cancel_appointment_reminders(self, appointment: Appointment):
-        # TODO: Schedule
-        return
+        for task_id in self.timer_tasks.keys():
+            if task_id.startswith(appointment.name):
+                self.timer_tasks.get(task_id).cancel()
+                self.timer_tasks.pop(task_id)
 
     def update_appointment_reminder(self, client_name: str, appointment: Appointment, reminder: str):
-        # TODO: Schedule
-        return
+        task_id = self.__get_timer_task_id(client_name, appointment.name)
+
+        if task_id in self.timer_tasks.keys():
+            self.timer_tasks.get(task_id).cancel()
+            self.timer_tasks.pop(task_id)
+
+        self.schedule_appointment_reminder(client_name, appointment, reminder)
 
     def send_appointment_invite(self, client_name: str, appointment: Appointment, signature: str):
         sse.publish({
